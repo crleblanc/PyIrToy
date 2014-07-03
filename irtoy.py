691efc5063eeb448b205e38e09b4373865f08dc5
Chris LeBlanc <crleblanc@gmail.com>
2014-07-03 15:11:52 +1200

    modified one of the tests to disable the handshake, transmit notify, and byte count checks.

--- irtoy.py
+++ irtoy.py
@@ -9,12 +9,12 @@
 #
 #--
 #
-# This work is free: you can redistribute it and/or modify it under the terms 
+# This work is free: you can redistribute it and/or modify it under the terms
 # of Creative Commons Attribution ShareAlike license v3.0
 #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-# See the License for more details. You should have received a copy of the License along 
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more details. You should have received a copy of the License along
 # with this program. If not, see <http://creativecommons.org/licenses/by-sa/3.0/>.
 
 import time
@@ -38,16 +38,18 @@ class IrToy(object):
         self.toy = serialDevice
 
         self.sleepTime = 0.05
+        self.checkHandshake = False
+        self.checkTransmitNotify = False
+        self.checkByteCount = False
         self.handshake = None
         self.byteCount = None
-        self.complete = None
 
         self.requiredVersion = 22
         hardware, revision = self.firmware_revision()
         if self.firmware_revision()[1] < self.requiredVersion:
             raise FirmwareVersionError("pyirtoy will only work with firmware version %d or greater, current=%d"
                                         % (self.requiredVersion, revision))
-        
+
         # always use sampling mode
         self._setSamplingMode()
 
@@ -59,7 +61,7 @@ class IrToy(object):
         self.reset()
         self.toy.write(b'v')
         self._sleep()
-        
+
         versionString = self.toy.read(4)
         hardwareVersion = int(versionString[1:2])
         firmwareVersion = int(versionString[2:4])
@@ -68,6 +70,43 @@ class IrToy(object):
 
     def _sleep(self):
         time.sleep(self.sleepTime)
+
+    def _writeList(self, code, check_handshake=False):
+        '''write a list like object of integer values, if check_handshake is true it returns that code,
+        otherwise returns None'''
+
+        self._sleep()
+
+        byteCode = bytearray(code)
+        bytesWritten = 0
+        handshake = None
+
+        # 31 * 2 bytes = max of 62 bytes in the buffer.  31 hangs so using 32, strange.
+        maxWriteSize = 32
+        for idx in range(0, len(code), maxWriteSize):
+            segmentWritten = self.toy.write(byteCode[idx:idx+maxWriteSize])
+            bytesWritten += segmentWritten
+
+            if check_handshake:
+                handshake = ord(self.toy.read(1))
+
+        if bytesWritten != len(code):
+            raise IOError("incorrect number of bytes written to serial device, expected %d" % len(code))
+            
+        return handshake
+
+    def _checkTransmitReport(self):
+        '''get the byteCount and completion status from the IR Toy'''
+
+        if self.checkByteCount:
+            hexBytes = binascii.b2a_hex(self.toy.read(3)[1:])
+            self.byteCount = int(hexBytes, 16)
+
+        if self.checkTransmitNotify:
+            complete = self.toy.read(1)
+
+            if complete not in [b'c', b'C']:
+                raise IRTransmitError("Failed to transmit IR code, report=%s" % complete)
 
     def _setSamplingMode(self):
         '''set the IR Toy to use sampling mode, which we use exclusively'''
@@ -81,46 +120,32 @@ class IrToy(object):
 
         self._sleep()
 
-    def _writeList(self, code, check_handshake=False):
-        '''write a list like object of integer values'''
+    def _setTransmitMode(self):
+        '''set the IR Toy to use transmit mode.  Used by the transmit method'''
 
         self._sleep()
 
-        byteCode = bytearray(code)
-        bytesWritten = 0
+        if self.checkHandshake:
+            # Enable transmit handshake
+            self._writeList([0x26])
 
-        # 31 * 2 bytes = max of 62 bytes in the buffer.  31 hangs so using 32, strange.
-        maxWriteSize = 32
-        for idx in range(0, len(code), maxWriteSize):
-            segmentWritten = self.toy.write(byteCode[idx:idx+maxWriteSize])
-            bytesWritten += segmentWritten
+        if self.checkTransmitNotify:
+            # Enable transmit notify on complete
+            self._writeList([0x25])
 
-            if check_handshake:
-                self.handshake = ord(self.toy.read(1))
+        if self.checkByteCount:
+            # Enable transmit byte count report
+            self._writeList([0x24])
 
-        if bytesWritten != len(code):
-            raise IOError("incorrect number of bytes written to serial device, expected %d" % len(code))
+        # TODO: look into check_handshake here, not 100% sure what this expects in this case
+        handshake = self._writeList([0x03], check_handshake=self.checkHandshake) #Expect to receive packets to transmit
 
-    def _getTransmitReport(self):
-        '''get the byteCount and completion status from the IR Toy'''
-
-        hexBytes = binascii.b2a_hex(self.toy.read(3)[1:])
-        self.byteCount = int(hexBytes, 16)
-        self.complete = self.toy.read(1)
-
-    def _setTransmit(self):
-        
-        self._sleep()
-        self._writeList([0x26]) #Enable transmit handshake
-        self._writeList([0x25]) #Enable transmit notify on complete
-        self._writeList([0x24]) #Enable transmit byte count report
-        self._writeList([0x03], check_handshake=True) #Expect to receive packets to transmit
         self.transmitMode = True
 
     def receive(self):
-        '''Read a signal from the toy, returns a list of IR Codes converted from hex to ints.  See 
+        '''Read a signal from the toy, returns a list of IR Codes converted from hex to ints.  See
         http://dangerousprototypes.com/docs/USB_IR_Toy:_Sampling_mode for more information on the
-        sample format.  Reading starts when an IR signal is received and stops after 1.7 seconds of 
+        sample format.  Reading starts when an IR signal is received and stops after 1.7 seconds of
         inactivity'''
 
         self._sleep()
@@ -156,10 +181,12 @@ class IrToy(object):
         self.transmitMode = False
         self._sleep()
 
-    def transmit(self, code):
+    def transmit(self, code, check_handshake=False, check_complete=False, check_byte_count=False):
         '''switch to transmit mode and write the list (or list-like) set of ints to the toy for transmission,
         Must be an even number of elements.  The list created by read() can be used for transmitting.  If the
         last two elements of the list are not 0xff these will be appended.'''
+
+        # TODO: add docstrings for kwargs
 
         if len(code) < 2:
             raise ValueError("Length of code argument must be greater than or equal to two")
@@ -171,22 +198,24 @@ class IrToy(object):
         if code[-2:] != [0xff, 0xff]:
             code.extend([0xff, 0xff])
 
+        self.checkHandshake = check_handshake
+        self.checkTransmitNotify = check_complete
+        self.checkByteCount = check_byte_count
+
         try:
             self._sleep()
-            self._setTransmit()
-            self._writeList(code, check_handshake=True)
+            self._setTransmitMode()
+            # Currently not using the handshake response
+            handshake = self._writeList(code, check_handshake=self.checkHandshake)
             self._sleep()
-            self._getTransmitReport()
-
-            if self.complete not in [b'c', b'C']:
-                raise IRTransmitError("Failed to transmit IR code, report=%s" % self.complete)
+            self._checkTransmitReport()
         except:
             # if anything went wrong then sheepishly try to reset state and raise the exception,
             # surprisingly common on a weak CPU like the raspberry pi
-            #self.toy.flushOutput() # hmm, maybe this will help? Interesting: we get a crazy state until a new program is started, then fine.
             self.reset()
             self._setSamplingMode()
             raise
 
-        # experimentation shows that returning to sampling mode is needed to avoid dropping the serial connection on Linux
+        # experimentation shows that returning to sampling mode is needed to avoid 
+        # dropping the serial connection on Linux
         self._setSamplingMode()
--- tests/test_irtoy.py
+++ tests/test_irtoy.py
@@ -9,16 +9,16 @@ import unittest
 from irtoy import IrToy
 
 class SerialMock(object):
-    '''A serial device stand-in class used to mock out the serial object so we can 
+    '''A serial device stand-in class used to mock out the serial object so we can
     see what was written, and fake something to be read'''
-    
+
     def __init__(self):
         self.writeHistory = []
         self.readCount = 0
         self.readCode = None
-        
+
     def write(self, code):
-        
+
         # code can be either strings or hex numbers, so convert bytearrays to ints
         # for ease of comparison
         convertedCode = None
@@ -26,31 +26,31 @@ class SerialMock(object):
             convertedCode = [int(x) for x in code]
         else:
             convertedCode = code
-            
+
         self.writeHistory.append(convertedCode)
 
         return len(code)
 
     def setReadCode(self, code):
-        '''set the code that the read() method will emit when called.  Each 
+        '''set the code that the read() method will emit when called.  Each
         call to read() will return the ith element in the supplied list'''
         self.readCode = code
 
     def read(self, nBytes):
-        
+
         if self.readCode:
             response = self.readCode[self.readCount]
             self.readCount += 1
         else:
             #response = b'S01'
             response = b'V222'
-        
+
         return response
-        
+
 
     def close(self):
         pass
-        
+
 
 class TestIrToy(unittest.TestCase):
 
@@ -64,14 +64,15 @@ class TestIrToy(unittest.TestCase):
 
         # length of code sent must be even
         self.assertRaises(ValueError, self.toy.transmit, [10])
-        
+
         # set the expected results from read(): protocol version, handshake ('>' converts to 62),
         # byte count transmitted, and completion code.
         self.serialMock.setReadCode([bytearray([62]), bytearray([62]), bytearray([0,0,0,1]), b'C', b'S01'])
-        
-        self.toy.transmit([10,10])
-        
-        # when transmitting, we expect a reset (five 0x00), 'S' to enter sampling mode, 0x26 for enable handshake,
+
+        # transmit using all the checks (even though they appear unreliable during buffer underruns)
+        self.toy.transmit([10,10], check_handshake=True, check_complete=True, check_byte_count=True)
+
+        # when transmitting, we expect three resets (three * five 0x00), 'S' to enter sampling mode, 0x26 for enable handshake,
         # 0x25 to enable notify on transmit, 0x24 to enable transmit byte count, and 0x03 to start the transmission,
         # then the list of codes to transmit (always ending with 0xff, 0xff), and another reset.  See DP link at top of this file for more info.
         expectedHistory = [[0x00, 0x00, 0x00, 0x00, 0x00],
@@ -86,9 +87,26 @@ class TestIrToy(unittest.TestCase):
 
         self.assertEqual(self.serialMock.writeHistory, expectedHistory)
 
+        # transmit again, but without the check
+        self.setUp()
+        self.serialMock.setReadCode([bytearray([62]), b'S01'])
+        self.toy.transmit([10,10])
+
+        expectedHistory = [[0x00, 0x00, 0x00, 0x00, 0x00],
+                           b'v',
+                           [0x00, 0x00, 0x00, 0x00, 0x00],
+                           b'v',
+                           [0x00, 0x00, 0x00, 0x00, 0x00],
+                           b'S', [0x03],
+                           [10, 10, 0xff, 0xff],
+                           [0x00, 0x00, 0x00, 0x00, 0x00],
+                           b'S']
+
+        self.assertEqual(self.serialMock.writeHistory, expectedHistory)
+
     def testReceive(self):
 
-        # pretend to be receiving the following signals from the IR Toy, must end with 0xff,0xff (same as 255, means no signal) 
+        # pretend to be receiving the following signals from the IR Toy, must end with 0xff,0xff (same as 255, means no signal)
         # or the code will keep recording indefinitely.  First element is for the protocol version, since receive() resets the toy
         self.serialMock.setReadCode([bytearray([62]), bytearray([1]), bytearray([2]), bytearray([0xff]), bytearray([0xff])])
         readSignal = self.toy.receive()
@@ -101,13 +119,13 @@ class TestIrToy(unittest.TestCase):
         # and the pySerial module which isn't necessarily installed by default.
 
         # IR code that causes an issue, any code would probably work for this purpose.
-        irCode=[1, 159, 0, 212, 0, 26, 0, 25, 0, 26, 0, 78, 0, 26, 0, 25, 0, 26, 0, 25, 0, 26, 0, 78, 0, 26, 0, 
+        irCode=[1, 159, 0, 212, 0, 26, 0, 25, 0, 26, 0, 78, 0, 26, 0, 25, 0, 26, 0, 25, 0, 26, 0, 78, 0, 26, 0,
                 25, 0, 27, 0, 77, 0, 26, 0, 77, 0, 26, 0, 78, 0, 26, 0, 26, 0, 26, 0, 78, 0, 26, 0, 78, 0, 26,
-                0, 25, 0, 26, 0, 78, 0, 26, 0, 78, 0, 26, 0, 24, 0, 28, 0, 76, 0, 27, 0, 25, 0, 27, 0, 77, 0, 27, 
-                0, 25, 0, 27, 0, 24, 0, 27, 0, 24, 0, 28, 0, 24, 0, 28, 0, 24, 0, 28, 0, 24, 0, 27, 0, 76, 0, 28, 
-                0, 24, 0, 27, 0, 77, 0, 27, 0, 76, 0, 27, 0, 76, 0, 28, 0, 76, 0, 27, 0, 76, 0, 28, 7, 15, 1, 160, 
+                0, 25, 0, 26, 0, 78, 0, 26, 0, 78, 0, 26, 0, 24, 0, 28, 0, 76, 0, 27, 0, 25, 0, 27, 0, 77, 0, 27,
+                0, 25, 0, 27, 0, 24, 0, 27, 0, 24, 0, 28, 0, 24, 0, 28, 0, 24, 0, 28, 0, 24, 0, 27, 0, 76, 0, 28,
+                0, 24, 0, 27, 0, 77, 0, 27, 0, 76, 0, 27, 0, 76, 0, 28, 0, 76, 0, 27, 0, 76, 0, 28, 7, 15, 1, 160,
                 0, 107, 0, 27, 255, 255] #Onkyo TX-SR508 mute
-        
+
         import serial
         serialDevice = serial.Serial('/dev/ttyACM0')
         #serialDevice = self.serialMock
@@ -116,7 +134,7 @@ class TestIrToy(unittest.TestCase):
         toy = IrToy(serialDevice)
         for i in range(5):
             toy.transmit(irCode)
-            
+
         # using a new object for each transmit
         for i in range(5):
             toy2 = IrToy(serialDevice)
